1d7ed292400414ab511bbfae063d105c54c0a8fe
Aryan Kumar <aryan8910kumar@gmail.com>
2024-05-12 02:50:06 +0530

    Add:Navbar UI

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -2,8 +2,9 @@
 import Link from "next/link";
 import { RiShoppingBag3Line } from "react-icons/ri";
 import { BiCategory } from "react-icons/bi";
-import { IoSearchSharp } from "react-icons/io5";
+import { IoSearchOutline } from "react-icons/io5";
 import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
 
 const Headers = () => {
   const [isShowCategory, setIsShowCategory] = useState(false);
@@ -25,28 +26,46 @@ const Headers = () => {
   return (
     <nav className="w-full flex items-center justify-between px-10 bg-white shadow h-20 text-xl">
       <div className="text-3xl font-medium display-flex gap-8">
-        <div className="display-flex gap-5 text-yellow-600 ">
-          <Link href={"/"}>
-            Grocery <span className="text-green-400">Store</span>
+        <div className="display-flex gap-1 text-yellow-600  ">
+          <span>
+            <Image
+              src={"/Images/Logoicon.png"}
+              alt="Logo"
+              width={50}
+              height={50}
+            />
+          </span>
+          <Link href={"/"} className="leading-7">
+            Grocery <br /> <span className="text-green-500">Store</span>
           </Link>
         </div>
         <div className="w-auto relative" ref={categoryRef}>
           <div
-            className="flex gap-3 bg-slate-300 py-2 px-10 rounded relative cursor-pointer"
+            className="display-flex gap-2 bg-slate-200 py-2 px-6 rounded-full relative cursor-pointer"
             onClick={IsShowCategorySection}>
-            <BiCategory />
-            <span className="text-xl">Category</span>
+            <BiCategory className="text-xl" />
+            <span className="text-lg">Category</span>
           </div>
           {isShowCategory && (
-            <div className="absolute top-12 left-0 right-0 bg-slate-300 h-80 w-52 rounded">
-              <h3>Hello</h3>
+            <div className="absolute top-12 left-1 right-0 bg-slate-100 border h-36 w-36 rounded">
+              <div className="text-base flex flex-col">
+                <h3 className="font-medium text-center py-1">
+                  Browse Category
+                </h3>
+                <div className="py-1 flex flex-col  items-start px-2">
+                  <p className="cursor-pointer text-base">Profile</p>
+                  <p className="cursor-pointer text-base">Biling</p>
+                  <p className="cursor-pointer text-base">Team</p>
+                  <p className="cursor-pointer text-base">Subscription</p>
+                </div>
+              </div>
             </div>
           )}
         </div>
-        <div className="w-96 py-2 px-4 bg-slate-300 rounded display-flex">
-          <IoSearchSharp />
+        <div className="w-72 py-1.5 px-4 bg-white border border-gray-500 rounded-full display-flex ">
+          <IoSearchOutline />
           <input
-            className="w-full h-full placeholder-black outline-none border-none bg-transparent text-xl px-2 text-black "
+            className="w-full h-full placeholder-black outline-none border-none bg-transparent text-lg px-2 text-black "
             placeholder="Search"
           />
         </div>
